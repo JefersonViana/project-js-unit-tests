bffa6850c0a0162ec0e10e754123f0935010e348
jefersonviana <jefersonv28@gmail.com>
2023-01-23 23:25:12 -0300

    vqv concluído

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,13 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
-
+const vqv = (nome, idade) => {
+  if (!nome || !idade) {
+    return undefined;
+  }
+  const parte1 = 'Oi, meu nome é';
+  const parte2 = 'trabalho na Trybe e mando muito em programação!';
+  return `${parte1} ${nome}!\nTenho ${idade} anos,\n${parte2}\n#VQV!`;
+};
+// console.log(vqv('Jeferson', 27));
 module.exports = vqv;
